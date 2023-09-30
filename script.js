abedb3add81670f93a4c8dac71781b5bf4279e59
ShevinuM <nsmcooray@mun.ca>
2023-09-30 18:07:44 -0230

    Implemented changing colours

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ document.addEventListener("DOMContentLoaded", function () {
 	const container = document.getElementById("grid-container");
 
 	let isMouseDown = false;
+	let penColor = "black"
+	let backgroundColor = "white";
 
 	// Event to set the flag true when mouse is down
 	container.addEventListener("mousedown", function () {
@@ -20,7 +22,43 @@ document.addEventListener("DOMContentLoaded", function () {
 		container.appendChild(cell);
 
 		cell.addEventListener("mouseover", function () {
-			if (isMouseDown) this.style.backgroundColor = "black";
+			if (isMouseDown) this.style.backgroundColor = penColor;
 		});
 	}
+
+	const penColorPicker = document.querySelector('#pen-color');
+	penColorPicker.addEventListener("change", watchPenColorPicker);
+
+	function watchPenColorPicker(event) {
+		penColor = event.target.value;
+	}
+
+	const backgroundColorPicker = document.querySelector('#background-color');
+	backgroundColorPicker.addEventListener("change", watchBackgroundColorPicker);
+
+	function watchBackgroundColorPicker(event) {
+		container.style.backgroundColor = event.target.value;
+	}
+
+	const gridColorPicker = document.querySelector('#grid-color');
+	gridColorPicker.addEventListener("change", watchGridColorPicker);
+
+	function watchGridColorPicker(event) {
+		document.querySelectorAll(".cell").forEach((p) => {
+			p.style['border-color'] = event.target.value;
+		  });
+		container.style['border-color'] = event.target.value;
+	}
+
+	document.getElementById("showGridLines").addEventListener("change", function() {
+		if (this.checked) {
+			document.querySelectorAll(".cell").forEach((p) => {
+				p.style['border-width'] = '0.5px';
+			  });
+		} else {
+			document.querySelectorAll(".cell").forEach((p) => {
+				p.style['border-width'] = '0px';
+			  });
+		}
+	  });
 });
